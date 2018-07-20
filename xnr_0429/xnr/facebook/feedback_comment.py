6239e856f90e368f4c3ac06ad3884578d7f132d1
BingquLee <bingqulee@gmail.com>
2018-07-20 11:24:45 +0800

    modify feedback_comment.py

--- xnr_0429/xnr/facebook/feedback_comment.py
+++ xnr_0429/xnr/facebook/feedback_comment.py
@@ -10,75 +10,81 @@ class Comment():
 	def __init__(self, username, password):
 		self.launcher = Launcher(username, password)
 		self.es = Es_fb()
-		self.comment_list,self.driver,self.display = self.launcher.get_comment_list()
+		self.comment_list,self.driver = self.launcher.get_comment_list()
 		self.list = []
 		self.update_time = int(time.time())
 
 	def get_comment(self):
-		try:
-			for url in self.comment_list:
-				self.driver.get(url)
-				time.sleep(1)
-				# 退出通知弹窗进入页面
+		print 00000000000000
+		print 'comment_list', self.comment_list
+		for url in self.comment_list:
+			self.driver.get(url)
+			time.sleep(1)
+			# 退出通知弹窗进入页面
+			print 11111111111111111111
+			try:
+				print 2222222222222222
+				self.driver.find_element_by_xpath('//div[@class="_n8 _3qx uiLayer _3qw"]').click()
+			except BaseException, e:
+				print "get_comment Position111111", e
+				pass
+
+			html = self.driver.page_source
+			with open('get_comment1111.html', 'wb') as f:
+				f.write(html)
+			try:
+				print 333333333333
+				root_text = self.driver.find_element_by_xpath('//div[@role="feed"]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]').text
+			except BaseException, e:
+				print "get_comment Position22222222222", e
+				root_text = self.driver.find_element_by_xpath('//div[@role="feed"]/div/div/div[3]/div[2]/form/div[2]/div/div[1]').text
+
+			
+			try:
+				print 44444444444444444
+				root_mid = ''.join(re.findall(re.compile('story_fbid=(\d+)&amp'),self.driver.page_source))
+				print 'root_mid', root_mid
+			except BaseException, e:
+				print "get_comment Position44444444", e 
+				root_mid = ''.join(re.findall(re.compile('story_fbid=(\d+)'),self.driver.find_element_by_xpath('//div[@role="feed"]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div/div/div[2]/div/div/div[2]/div/span[3]/span/a').get_attribute('href')))
+
+			for each in self.driver.find_elements_by_xpath('//div[@aria-label="评论"]'):
+				
 				try:
-					self.driver.find_element_by_xpath('//div[@class="_n8 _3qx uiLayer _3qw"]').click()
+					print 5555555555555
+					author_name = each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/div/div/span/span[1]/a').text
 				except BaseException, e:
-					print "get_comment Position111111", e
-					pass
+					print "get_comment Position66666666", e
+					author_name = each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/span/span[1]/a').text					
+
+
+
+				
+				try:
+					print 6666666666
+					author_id = ''.join(re.findall(re.compile('id=(\d+)'),each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/div/div/span/span[1]/a').get_attribute('data-hovercard')))
+				except:
+					author_id = ''.join(re.findall(re.compile('id=(\d+)'),each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/span/span[1]/a').get_attribute('data-hovercard')))					
 
 				try:
-					try:
-						root_text = self.driver.find_element_by_xpath('//div[@role="feed"]/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]').text
-					except BaseException, e:
-						print "get_comment Position22222222222", e
-						root_text = self.driver.find_element_by_xpath('//div[@role="feed"]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]').text
-				except BaseException, e:
-					print "get_comment Position3333333333", e
-					root_text = 'None'
+					print 7777777777
+					pic_url = each.find_element_by_xpath('./div/div/div/div[1]/a/img').get_attribute('src')
+				except:
+					pic_url = 'None'
 				try:
-					try:
-						root_mid = ''.join(re.findall(re.compile('story_fbid=(\d+)'),self.driver.find_element_by_xpath('//div[@role="feed"]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div/div/div[2]/div/div/div[2]/div/span[3]/span/a').get_attribute('href')))
-					except BaseException, e:
-						print "get_comment Position44444444", e 
-						root_mid = ''.join(re.findall(re.compile('story_fbid=(\d+)'),self.driver.find_element_by_xpath('//div[@role="feed"]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div/div/div[2]/div/div/div[2]/div/span[3]/span/a').get_attribute('href')))
-				except BaseException, e:
-					print "get_comment Position5555555555", e
-					root_mid = 'None'
-				for each in self.driver.find_elements_by_xpath('//div[@aria-label="评论"]'):
-					try:
-						try:
-							author_name = each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/div/div/span/span[1]/a').text
-						except BaseException, e:
-							print "get_comment Position66666666", e
-							author_name = each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/span/span[1]/a').text					
-					except BaseException, e:
-						print "get_comment Position77777777", e
-						author_name = 'None'
-					try:
-						try:
-							author_id = ''.join(re.findall(re.compile('id=(\d+)'),each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/div/div/span/span[1]/a').get_attribute('data-hovercard')))
-						except:
-							author_id = ''.join(re.findall(re.compile('id=(\d+)'),each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/span/span[1]/a').get_attribute('data-hovercard')))					
-					except:	
-						author_id = 'None'
-					try:
-						pic_url = each.find_element_by_xpath('./div/div/div/div[1]/a/img').get_attribute('src')
-					except:
-						pic_url = 'None'
-					try:
-						content = each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/div/div/span/span[2]/span/span/span/span').text
-					except:
-						content = each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/span/span[2]/span/span/span/span').text					
-					try:
-						ti = int(each.find_element_by_xpath('./div/div/div/div[2]/div/div/div[2]/span[4]/a/abbr').get_attribute('data-utime'))
-					except:
-						ti = int(each.find_element_by_xpath('./div/div/div/div[2]/div/div/div[2]/span[5]/a/abbr').get_attribute('data-utime'))					
-					self.list.append({'uid':author_id, 'photo_url':pic_url, 'nick_name':author_name, 'mid':root_mid, 'timestamp':ti, 'text':content,\
+					print 888888888
+					content = each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/div/div/span/span[2]/span/span/span/span').text
+				except:
+					content = each.find_element_by_xpath('./div/div/div/div[2]/div/div/div/span/span[2]/span/span/span/span').text					
+				try:
+					print 99999999
+					ti = int(each.find_element_by_xpath('./div/div/div/div[2]/div/div/div[2]/span[4]/a/abbr').get_attribute('data-utime'))
+				except:
+					ti = int(each.find_element_by_xpath('./div/div/div/div[2]/div/div/div[2]/span[5]/a/abbr').get_attribute('data-utime'))					
+				self.list.append({'uid':author_id, 'photo_url':pic_url, 'nick_name':author_name, 'mid':root_mid, 'timestamp':ti, 'text':content,\
 										 'update_time':self.update_time, 'root_text':root_text, 'root_mid':root_mid})
-		finally:
+
 			self.driver.quit()
-			# self.launcher.display.stop()
-			self.display.popen.kill()
 		return self.list
 
 	def save(self,indexName,typeName,list):
@@ -86,8 +92,8 @@ class Comment():
 
 if __name__ == '__main__':
 	# comment = Comment('8618348831412','Z1290605918')
-	#comment = Comment('8613520874771','13018119931126731x')
-	comment = Comment('8613269704912', 'chenhuiping')
+	comment = Comment('8613520874771','13018119931126731x')
+	#comment = Comment('+8613269704912', 'chenhuiping')
 	list = comment.get_comment()
 	print list
 	#self.display.popen.kill()
